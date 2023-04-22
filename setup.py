6ae922c0250c98cd1f72c504d8f721cb24d25b0b
igormusic <igormusich@gmail.com>
2023-04-22 02:27:57 -0400

    add long description

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
 setup(
     name='transaction-accounts',
-    version='0.0.1',
+    version='0.0.2',
     description='Create configuration for transactional accounts and implement account runtime',
+    long_description=long_description,
     author='Igor Music',
     author_email='igormusich@gmail.com',
     packages=find_packages(),
     license='MIT',
     url='https://github.com/igormusic/transaction-accounts',
-    download_url='https://github.com/igormusic/transaction-accounts/archive/refs/tags/0.0.1.tar.gz',
+    download_url='https://github.com/igormusic/transaction-accounts/archive/refs/tags/0.0.2.tar.gz',
     keywords=['TRANSACTION PROCESSING', 'LOANS', 'SAVINGS', 'ACCOUNTS', 'FINANCE', 'BANKING'],
     install_requires=[
         'python-dateutil',
